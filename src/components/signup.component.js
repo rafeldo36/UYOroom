a2bc91d0c75fb93664002dc76a7ad7776aac3314
Rafey <rafeldo36@gmail.com>
2024-06-03 17:34:56 +0530

    donee

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -7,7 +7,7 @@ const SignUp = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { name, email, password } = credentials;
-        const response = await fetch('http://localhost:5000/api/auth/createuser', {
+        const response = await fetch('https://uyoroom.onrender.com/api/auth/createuser', {
             method: 'POST', 
             mode:"cors",
             headers: {

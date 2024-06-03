4df8d843a561448d16781cb2cbf72325910c1cf4
Rafey <rafeldo36@gmail.com>
2024-06-03 17:32:37 +0530

    secret updated

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb+srv://rafeldo36:Noori@36@cluster0.kfaxzjs.mongodb.net/?retryWrites=true&w=majority'
+const mongoURI = 'mongodb+srv://rafeldo36:Noori36@cluster0.kfaxzjs.mongodb.net/?retryWrites=true&w=majority'
 
 const connectToMongo = () => {
 
--- src/components/login.component.js
+++ src/components/login.component.js
@@ -7,7 +7,7 @@ let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://uyoroom.onrender.com/api/auth/login", {
       method: 'POST',
       mode:"cors",
       headers: {
